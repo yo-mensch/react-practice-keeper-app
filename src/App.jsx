83f3eecc86c5928c15f4d8278179d9247617c491
yo-mensch <rasa.j1225@gmail.com>
2021-03-11 11:56:42 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../src/Header'
+import Footer from '../src/Footer'
+import Note from '../src/Note'
+
+function App(){
+    return(
+        <div>
+            <Header/>
+            <Note/>
+            <Footer/>
+        </div>
+    )
+}
+export default App;
