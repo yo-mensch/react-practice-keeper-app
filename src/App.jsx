e4ba1be791720c549f15f2c144cde5ec1661ac12
yo-mensch <rasa.j1225@gmail.com>
2021-03-12 08:55:10 +0200

    Impemented mapping through notes array

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Header from '../src/Header'
 import Footer from '../src/Footer'
 import Note from '../src/Note'
 
+import notes from '../src/notes'
+
 function App(){
     return(
         <div>
             <Header/>
-            <Note name = "Note 1" content = "This is the content of Note 1"/>
-            <Note name = "Note 2" content = "Eins Zwei Polizei"/>
-            <Note name = "Note 3" content = "Drei Vier Grenadier"/>
-            <Note name = "Note 4" content = "Fünf Sechs, Alte Hex"/>
-            <Note name = "Note 5" content = "Sieben Acht, Gutenacht"/>
+            {notes.map(note => <Note
+                name = {note.name}
+                content = {note.content}
+            />)}
             <Footer/>
         </div>
     )
---
+++ src/notes.js
@@ -0,0 +1,20 @@
+let notes = [
+    {
+        name: "Eins, Zwei",
+        content: "Polizei"
+    },
+    {
+        name: "Drei, Vier",
+        content: "Grenadier"
+    },
+    {
+        name: "Fünf, Sechs",
+        content: "Alte Hex"
+    },
+    {
+        name: "Sieben, Acht",
+        content: "Gutenacht"
+    }
+]
+
+export default notes;
