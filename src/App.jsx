f5db8c3db0881f315043a873278fa35127bccb22
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2021-05-04 12:33:59 +0300

    add input area

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../src/Header'
 import Footer from '../src/Footer'
 import Note from '../src/Note'
+import CreateArea from '../src/CreateArea'
 
 import notes from '../src/notes'
 
@@ -9,6 +10,7 @@ function App(){
     return(
         <div>
             <Header/>
+            <CreateArea/>
             {notes.map(note => <Note
                 key = {note.id}
                 name = {note.name}
--- src/Note.jsx
+++ src/Note.jsx
@@ -5,6 +5,7 @@ function Note(props){
         <div className="note">
             <h1>{props.name}</h1>
             <p>{props.content}</p>
+            <button>DELETE</button>
         </div>
     )
 }
